88c4da98aa662110209ba3d0b2c09b323ee65624
eklavya007 <75568316+eklavya007@users.noreply.github.com>
2021-08-02 12:40:27 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var  database;
+
+var gameState=0
+var playerCount
+var canvas,form,player,game
+
+
+function setup(){
+  canvas=createCanvas(400,400)
+  database = firebase.database();
+  game= new Game()
+  game.getState()
+ game.start()
+}
+
+function draw(){
+  background("white");
+  
+   
+  
+}
